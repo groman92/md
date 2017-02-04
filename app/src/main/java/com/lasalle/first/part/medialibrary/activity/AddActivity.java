b02a9781b0f419b53fddb27e6ef8fbde11b97617
Gerard Román <ls24381@salleurl.edu>
2017-02-04 22:24:08 +0100

    Modificat el gradle per poder afegir el TabLayout

--- app/src/main/java/com/lasalle/first/part/medialibrary/activity/AddActivity.java
+++ app/src/main/java/com/lasalle/first/part/medialibrary/activity/AddActivity.java
@@ -1,16 +1,22 @@
 package com.lasalle.first.part.medialibrary.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+
 
 import com.lasalle.first.part.medialibrary.R;
 
 public class AddActivity extends ToolbarAbstractActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.activity_add);
 
-        //Com és crida? on és fa el onCreate?
+
+        //TODO: que torni enrere..
         enableToolbarHomebutton();
+
+
+
+
     }
 }
