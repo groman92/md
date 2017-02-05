da2200013678e51e04648a0a427e855f3c2f65fc
Gerard Román <ls24381@salleurl.edu>
2017-02-05 02:32:09 +0100

    Neteja codi dels fragments AddMovie i AddTvshow

--- app/src/main/java/com/lasalle/first/part/medialibrary/fragment/AddTvshowFragment.java
+++ app/src/main/java/com/lasalle/first/part/medialibrary/fragment/AddTvshowFragment.java
@@ -11,18 +11,24 @@ import android.view.ViewGroup;
 import com.lasalle.first.part.medialibrary.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddTvshowFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
+
 public class AddTvshowFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
 
     public AddTvshowFragment() {
         // Required empty public constructor
+    }
+
+    public static AddTvshowFragment newInstance() {
+
+        AddTvshowFragment fragment = new AddTvshowFragment();
+
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 
 
@@ -33,42 +39,6 @@ public class AddTvshowFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_add_tvshow, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
